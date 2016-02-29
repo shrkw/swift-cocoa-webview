95877a56aa0c828c87c97cc852f10dd46b9a2824
shrkw <shrkwh@gmail.com>
2016-02-29 23:26:32 +0900

    Initial Commit

---
+++ WebView/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  WebView
+//
+//  Created by 白川 博之 on 2016/02/29.
+//  Copyright © 2016年 Hiroyuki Shirakawa. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ WebView/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  WebView
+//
+//  Created by 白川 博之 on 2016/02/29.
+//  Copyright © 2016年 Hiroyuki Shirakawa. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: AnyObject? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
