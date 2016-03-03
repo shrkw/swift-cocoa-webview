ea90bcbaed4d7f083da72234e1d16a8a6c8ced95
shrkw <shrkwh@gmail.com>
2016-03-04 02:21:07 +0900

    show url

--- WebView/ViewController.swift
+++ WebView/ViewController.swift
@@ -7,13 +7,43 @@
 //
 
 import Cocoa
+import WebKit
 
 class ViewController: NSViewController {
 
+    @IBOutlet weak var webView: WebView!
+    @IBOutlet weak var urlField: NSTextFieldCell!
+    
+    @IBAction func connect(sender: AnyObject) {
+        connectWebPage(self.urlField.stringValue)
+    }
+    
+    @IBAction func bake(sender: AnyObject) {
+        let url = "https://www.youtube.com/watch?v=eSEh70joiEQ"
+        self.urlField.stringValue = url
+            connectWebPage(url)
+    }
+    
+    func connectWebPage(urlStr: String) {
+        let url = NSURL(string: urlStr)
+        let req = NSURLRequest(URL: url!)
+        webView.mainFrame.loadRequest(req)
+    }
+    
+    let defaultUrl = "http://viibar.com"
+
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        // httpだとapp transport securityが発生する
+        self.urlField.stringValue = defaultUrl
+        connectWebPage(defaultUrl)
+        
+//        self.webBrowser.append(browser)
+//        self.tabPages.append(self.about_blank)
+        
+//        self.about_blank.view = browser
+        
+//        self.getCurrentWebBrowser().mainFrame.loadRequest(req)
     }
 
     override var representedObject: AnyObject? {
@@ -22,6 +52,10 @@ class ViewController: NSViewController {
         }
     }
 
+    
+    func getNewBrowser() -> WebView {
+        return WebView()
+    }
 
 }
 
